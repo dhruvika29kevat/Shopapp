34416708a9a4945c774de3939a3416bef5bae9c2
dhruvika.k@simformsolutions.com <dhruvika.k@simformsolutions.com>
2021-10-01 12:17:17 +0530

    Product, Category MVC created

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # get 'categories/show'
+  # get 'products/index'
+  # get 'products/show'
+  resources :products, only: [:index, :show]
+  resources :categories, only: [:show]
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
---
+++ db/migrate/20211001060837_create_products.rb
@@ -0,0 +1,11 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :name, null: false
+      t.text :description, null: false
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
