3ed3bd93c52487911e02584d353c7bc3f43bb33d
dhruvika.k@simformsolutions.com <dhruvika.k@simformsolutions.com>
2021-10-04 17:22:32 +0530

    added userlogin and cart functionality quantity function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'carts/show'
+  devise_for :users
+  devise_for :admins
   # get 'categories/show'
   # get 'products/index'
   # get 'products/show'
@@ -6,6 +9,15 @@ Rails.application.routes.draw do
   root to: 'products#index'
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
-
+  resource :cart, only: %i[:show] do
+    put 'add/:product_id', to: 'carts#add', as: :add_to
+    put 'remove/:product_id', to: 'carts#remove', as: :remove_from
+    put 'remove_one/:product_id', to: 'carts#removeone', as: :remove_one
+  end
+  namespace :admin do
+    root to: 'products#index'
+    resources :products, only: [:index, :new, :create, :edit, :update, :destroy]
+    resources :categories, only: [:index, :new, :create, :edit, :update, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
